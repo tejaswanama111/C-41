93281cb202cd227f859fe8817962f680c372eed0
tejaswanama111 <tejaswanama2006@gmail.com>
2020-11-03 19:40:40 -0800

    game completed

--- js/Game.js
+++ js/Game.js
@@ -1,6 +1,8 @@
 class Game {
   constructor(){
 
+
+
   }
 
   getState(){
@@ -44,6 +46,8 @@ class Game {
     form.hide();
     
     Player.getPlayerInfo();
+
+    player.getCarsAtEnd();
     
     if(allPlayers !== undefined){
       background(rgb(198,135,103));
@@ -93,6 +97,8 @@ class Game {
 
     if(player.distance > 3860){
       gameState = 2;
+      player.rank +=1;
+      Player.updateCarsAtEnd(player.rank);
     }
    
     drawSprites();
@@ -100,5 +106,6 @@ class Game {
 
   end(){
     console.log("Game Ended");
+    console.log(player.rank);
   }
 }
